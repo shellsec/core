af8ecf9fc6ff6e267c13455e0e023f278624d01e
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2018-06-07 20:38:06 +0300

    Pass links into template via OC_Defaults

--- core/templates/plain.mail.footer.php
+++ core/templates/plain.mail.footer.php
@@ -1,3 +1,11 @@
 --
 <?php p($theme->getName() . ' - ' . $theme->getSlogan()); ?>
-<?php print_unescaped("\n".$theme->getBaseUrl());
+<?php
+print_unescaped("\n" . $theme->getBaseUrl());
+if ($theme->getImprintUrl() !== '') {
+	print_unescaped("\n" . $theme->getImprintUrl());
+}
+if ($theme->getPrivacyPolicyUrl() !== '') {
+	print_unescaped("\n" . $theme->getPrivacyPolicyUrl());
+}
+?>
